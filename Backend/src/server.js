2e49beb3a64ad6e6208771fddb4a1600b40ad6bd
Subhra sundar sinha <subhrasundarsinha21@gmail.com>
2025-12-02 10:00:43 +0530

    feat: implement initial server setup with Express, database connection, middleware, and API routes.

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,15 +3,21 @@ import dotenv from "dotenv";
 import { initDB } from "./config/db.js";
 import { rateLimiter } from "./middleware/rateLimiter.js";
 import transactionRouter from "./routes/transactionRouter.js";
+import job from "./config/corn.js";
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 app.use(rateLimiter);
+if (process.env.NODE_ENV === "production") job.start();
 
 const PORT = process.env.PORT || 5003;
 
 app.use("/api/v1/transactions", transactionRouter);
+
+app.get("/health", (req, res) => {
+  res.json({ message: "Server is running" });
+});
 
 initDB().then(() => {
   app.listen(PORT, () => {
